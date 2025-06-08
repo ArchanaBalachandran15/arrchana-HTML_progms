dbdb174c46e8a1c53110f48692f464af9010eb40
Archana Balachandran <archanabalachandran2020@gmail.com>
2025-06-08 17:34:29 +0530

    Add files via upload

---
+++ js1.js
@@ -0,0 +1,38 @@
+//alert("hello world")
+//document.writeln("hello world")
+//document.getElementById("head1").innerHTML="WELCOME"
+//console.log("Hello World")
+
+//var a=document.getElementById("txt1").values;
+//var b=document.getElementById("txt2").values;
+//var res=parseInt(a)+parseInt(b)
+//document.writeln(a-b)
+//document.writeln(a*b)
+//document.writeln(a%b)
+
+//var age=25;
+//if(age>=18)
+    //{
+  //      alert("Eligible");
+   // }
+//else{
+        //alert("Not Eligible");
+    //}
+
+//var num=25;
+//if (num>0)
+//{
+    //alert("Positive")
+//}
+//else{
+    //alert("Negative")
+//}
+//else
+//{
+    //alert("Zero")
+//}
+
+
+    
+
+
